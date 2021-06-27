92ecacdaecea007d4f712d9c109b7033d29eb6ff
Saikot1997 <saikot-das.joy@stud.hshl.de>
2021-06-27 19:17:56 +0200

    Create robot_GoodCodePreachers.h

---
+++ Code Task 3 - algorithm 1 (wavefront)/robot_GoodCodePreachers.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef ROBOT_H
+#define ROBOT_H
+
+// Function declaration
+int move(char *world);
+int goingArray1(int*, char*);
+int goingArray2(int*, char*, int);
+
+
+int returning1(int*, char*);
+int returning2(int*, char*, int);
+
+
+#endif // ROBOT_H
